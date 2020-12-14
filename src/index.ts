97bd3f2f3552c79463ce2d0ef88064c2a363a9a9
jiangWeixian <jiangweixian1994@gmail.com>
2020-12-15 00:22:35 +0800

    ✅ init test startup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-const welcome = () => {
-  console.log('hello world')
+import { Dayjs, PluginFunc } from 'dayjs'
+
+type CalendarSetProps = {
+  month: number
 }
 
-export default welcome
+export const calendarSet: PluginFunc<undefined> = (_options, dayjs) => {
+  (dayjs.prototype as any).calendarSet = function (this: Dayjs, args: CalendarSetProps) {
+    const len = this.set('date', 1).set('month', args.month).daysInMonth()
+    const days = new Array(len).fill(0).map((_v, i) => this.date(i+1).format('YYYY-MM-DD'))
+    const first = this.month(args.month).startOf('month').day() - 1
+    const empties = new Array(first).fill('')
+    return empties.concat(days)
+  }
+}
